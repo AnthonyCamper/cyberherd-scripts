651ec3af05bab26f771da958069eec1238adb886
gerbsec <gerbsec@proton.me>
2024-02-04 00:23:46 -0500

    asdf

--- seccdc/linux/runWaseemNixArmorFork.sh
+++ seccdc/linux/runWaseemNixArmorFork.sh
@@ -1,2 +1,2 @@
-git clone https://github.com/gerbsec/Nixarmor-But-Better.git
+git clone https://github.com/gerbsec/Nixarmor-But-Better.git && git checkout ccdc && cd Nixarmor-But-Better
 sudo bash init.sh
