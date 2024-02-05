029fb74274b4f830eeb537391e29edddfc8a62cd
gerbsec <gerbsec@proton.me>
2024-02-05 12:19:13 -0500

    df

--- herdening/windows/2-passwordandsshlocalusers.ps1
+++ herdening/windows/2-passwordandsshlocalusers.ps1
@@ -2,8 +2,8 @@
 # TODO: generate CSVs for the password changes according to the specified format (https://anonymfile.com/6Q6D/seccdc-24-team-packet.pdf)
 
 # List of all scored admin/normal users.  Separated for ease of use later on
-$AdminUsers=@("elara.boss" "sarah.lee" "lisa.brown" "michael.davis" "emily.chen" "tom.harris" "bob.johnson" "david.kim" "rachel.patel" "dave.grohl" "kate.skye" "leo.zenith" "jack.rover")
-$NormalUsers=@("lucy.nova" "xavier.blackhole" "ophelia.redding" "marcus.atlas" "yara.nebula" "parker.posey" "maya.star" "zachary.comet" "quinn.jovi" "nina.eclipse" "alice.bowie" "ruby.rose" "owen.mars" "bob.dylan" "samantha.stephens" "parker.jupiter" "carol.rivers" "taurus.tucker" "rachel.venus" "emily.waters" "una.veda" "ruby.starlight" "frank.zappa" "ava.stardust" "samantha.aurora" "grace.slick" "benny.spacey" "sophia.constellation" "harry.potter" "celine.cosmos" "tessa.nova" "ivy.lee" "dave.marsden" "thomas.spacestation" "kate.bush" "emma.nova" "una.moonbase" "luna.lovegood" "frank.astro" "victor.meteor" "mars.patel" "grace.luna" "wendy.starship" "neptune.williams" "henry.orbit" "ivy.starling")
+$AdminUsers=@("elara.boss", "sarah.lee", "lisa.brown", "michael.davis", "emily.chen", "tom.harris", "bob.johnson", "david.kim", "rachel.patel", "dave.grohl", "kate.skye", "leo.zenith", "jack.rover")
+$NormalUsers=@("lucy.nova", "xavier.blackhole", "ophelia.redding", "marcus.atlas", "yara.nebula", "parker.posey", "maya.star", "zachary.comet", "quinn.jovi", "nina.eclipse", "alice.bowie", "ruby.rose", "owen.mars", "bob.dylan", "samantha.stephens", "parker.jupiter", "carol.rivers", "taurus.tucker", "rachel.venus", "emily.waters", "una.veda", "ruby.starlight", "frank.zappa", "ava.stardust", "samantha.aurora", "grace.slick", "benny.spacey", "sophia.constellation", "harry.potter", "celine.cosmos", "tessa.nova", "ivy.lee", "dave.marsden", "thomas.spacestation", "kate.bush", "emma.nova", "una.moonbase", "luna.lovegood", "frank.astro", "victor.meteor", "mars.patel", "grace.luna", "wendy.starship", "neptune.williams", "henry.orbit", "ivy.starling")
 
 #Create Arrays to hold local and domain users information
 $LocalUsers=@()
