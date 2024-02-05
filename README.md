aeeb9d5445645110a8d87cba74bc2d4ba2505c0f
gerbsec <gerbsec@proton.me>
2024-02-05 12:12:09 -0500

    okay

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # cyberherd-scripts
 
 Includes hardening/setup scripts for various competitions.
+
+
+Notes from AMA:
+
+IPs will be given through announcement, whitelist
+Red teams will not come from those IPS
+
+MySQL will use an out of scope user, everything else could be exploit it. (seccdc_black)
+
+No Amazon linux
+
+REPORT ALL PASSWORD CHANGES FOR ALL USERS, NOT JUST NON-ADMINS IN THE LIST. Upload CSV to inject portal. Notify people on mattermost.
+
--- herdening/linux/2-passwordAndSSHRotate.sh
+++ herdening/linux/2-passwordAndSSHRotate.sh
@@ -16,7 +16,52 @@ dave.grohl
 kate.skye
 leo.zenith
 jack.rover
-root
+lucy.nova
+xavier.blackhole
+ophelia.redding
+marcus.atlas
+yara.nebula
+parker.posey
+maya.star
+zachary.comet
+quinn.jovi
+nina.eclipse
+alice.bowie
+ruby.rose
+owen.mars
+bob.dylan
+samantha.stephens
+parker.jupiter
+carol.rivers
+taurus.tucker
+rachel.venus
+emily.waters
+una.veda
+ruby.starlight
+frank.zappa
+ava.stardust
+samantha.aurora
+grace.slick
+benny.spacey
+sophia.constellation
+harry.potter
+celine.cosmos
+tessa.nova
+ivy.lee
+dave.marsden
+thomas.spacestation
+kate.bush
+emma.nova
+una.moonbase
+luna.lovegood
+frank.astro
+victor.meteor
+mars.patel
+grace.luna
+wendy.starship
+neptune.williams
+henry.orbit
+ivy.starling
 )
 
 userIsAdmin() {
@@ -58,7 +103,7 @@ getent passwd | while IFS=: read -r username password uid gid full home shell; d
             echo "$username:$sharedPassphrase" | chpasswd
             if [ $? -eq 0 ]; then
                 echo "Password changed for $username"
-                if ! userIsAdmin "$username"; then
+                if userIsAdmin "$username"; then
                     echo "HOSTNAME-SERVICE,$username,$sharedPassphrase" >> "$outputFile"
                 fi
             else
