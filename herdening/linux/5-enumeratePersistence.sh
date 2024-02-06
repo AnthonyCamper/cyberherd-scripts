33995aababe39a876d658a0ae63e6aa15c5d9651
gerbsec <gerbsec@proton.me>
2024-02-05 20:10:41 -0500

    fuck

--- herdening/linux/5-enumeratePersistence.sh
+++ herdening/linux/5-enumeratePersistence.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
 # The Goal of this script is to check common directories for common reverse shells.
-echo "########################################## Checking HOME DIRECTORIES #################################################"
-grep -RIlP '(/dev/(tcp|udp)/.+|nc(\.exe)? -e sh|mkfifo /tmp/|exec [0-9]<>|perl -e|php -r|python(3)? -c|ruby -rsocket|socat|sqlite3 /dev/null|\$\(mktemp -u\)|zsh -c|lua(5\.1)? -e|go run|v run|awk \'BEGIN').*\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b' /home 2>/dev/null
-echo "########################################## Checking SERVICES DIRECTORIES #################################################"
-grep -RIlP '(/dev/(tcp|udp)/.+|nc(\.exe)? -e sh|mkfifo /tmp/|exec [0-9]<>|perl -e|php -r|python(3)? -c|ruby -rsocket|socat|sqlite3 /dev/null|\$\(mktemp -u\)|zsh -c|lua(5\.1)? -e|go run|v run|awk \'BEGIN').*\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b' /etc/systemd/ 2>/dev/null
-echo "########################################## Checking /var/www DIRECTORIES #################################################"
-grep -RIlP '(/dev/(tcp|udp)/.+|nc(\.exe)? -e sh|mkfifo /tmp/|exec [0-9]<>|perl -e|php -r|python(3)? -c|ruby -rsocket|socat|sqlite3 /dev/null|\$\(mktemp -u\)|zsh -c|lua(5\.1)? -e|go run|v run|awk \'BEGIN').*\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b' /var/www 2>/dev/null
-echo "########################################## Checking /root DIRECTORIES #################################################"
-grep -RIlP '(/dev/(tcp|udp)/.+|nc(\.exe)? -e sh|mkfifo /tmp/|exec [0-9]<>|perl -e|php -r|python(3)? -c|ruby -rsocket|socat|sqlite3 /dev/null|\$\(mktemp -u\)|zsh -c|lua(5\.1)? -e|go run|v run|awk \'BEGIN').*\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b' /root 2>/dev/null
-echo "########################################## Checking /etc DIRECTORIES #################################################"
-grep -RIlP '(/dev/(tcp|udp)/.+|nc(\.exe)? -e sh|mkfifo /tmp/|exec [0-9]<>|perl -e|php -r|python(3)? -c|ruby -rsocket|socat|sqlite3 /dev/null|\$\(mktemp -u\)|zsh -c|lua(5\.1)? -e|go run|v run|awk \'BEGIN').*\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b' /etc 2>/dev/null
+cmd_pattern='(curl|wget|bash|sh|zsh|mkfifo|python|perl|ruby|nc|netcat)'
+ip_pattern='([0-9]{1,3}[.]){3}[0-9]{1,3}'
+
+possible_backdoors() {
+    local file=$1
+    if awk -v cmd="$cmd_pattern" -v ip="$ip_pattern" '$0 ~ cmd && $0 ~ ip { found=1; exit } END { if (found) print FILENAME " contains suspicious patterns." }' "$file"; then
+        :
+    fi
+}
+
+directories=(
+    /etc/
+    /home
+    /root
+    /var/
+    /tmp
+    /dev/shm
+)
+
+for dir in directories; do
+    find dir -type f | while read -r file; do
+        possible_backdoors "$file"
+    done
+done
+
+echo "Cron cleanup completed."
