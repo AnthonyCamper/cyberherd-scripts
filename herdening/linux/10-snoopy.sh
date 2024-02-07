0d3e79043305597e54e530d5a86bd1cb304da582
Brandon Gonzalez <me@brandongonzalez.net>
2024-02-06 20:17:54 -0500

    Update 10-snoopy.sh

--- herdening/linux/10-snoopy.sh
+++ herdening/linux/10-snoopy.sh
@@ -31,4 +31,5 @@ get_snoopy_log_file() {
 SNOOPY_LOG_FILE=$(get_snoopy_log_file)
 
 echo "Tailing the Snoopy log file: $SNOOPY_LOG_FILE"
-sudo tail -f "$SNOOPY_LOG_FILE" | grep snoopy
+
+sudo tail -f "$SNOOPY_LOG_FILE" | grep snoopy | grep -vE "grep -qE|grep -qx|grep -qw|bash 8-pkillBash.sh|bash 4-ensureCorrectUsers.sh centos|grep -E (nc|netcat|bash|sh|zsh|mkfifo|python|perl|ruby|wget|curl)"
