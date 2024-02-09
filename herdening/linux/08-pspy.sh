5c6e901875a7f78dcfe67ba64efb4c5d7d8aa1bd
jcain7092 <106358431+jcain7092@users.noreply.github.com>
2024-02-08 19:10:16 -0500

    Update 08-pspy.sh

--- herdening/linux/08-pspy.sh
+++ herdening/linux/08-pspy.sh
@@ -2,4 +2,4 @@
 
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64
 chmod +x pspy64
-./pspy64 | grep -vE "grep -qE|grep -qx|grep -qw|bash 8-pkillBash.sh|bash 4-ensureCorrectUsers.sh centos|grep -E (nc|netcat|bash|sh|zsh|mkfifo|python|perl|ruby|wget|curl)"
+./pspy64 | grep -vE "grep -qE|grep -qx|grep -qw|bash pkillBash.sh|bash ensureCorrectUsers.sh centos|grep -E (nc|netcat|bash|sh|zsh|mkfifo|python|perl|ruby|wget|curl)"
