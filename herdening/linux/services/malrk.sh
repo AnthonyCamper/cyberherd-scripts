6969e3c2377e3a8bd8639a9281c723a12f7131aa
Brandon Gonzalez <me@brandongonzalez.net>
2024-02-18 16:14:17 -0500

    Update malrk.sh

--- herdening/linux/services/malrk.sh
+++ herdening/linux/services/malrk.sh
@@ -20,32 +20,16 @@ fi
 if [ "$operatingSystem" = "debian" ] || [ "$operatingSystem" = "ubuntu" ]; then
     echo "$operatingSystem detected, using apt"
     sudo apt install debsums -y
-    sudo apt install rkhunter -y 
-    sudo apt install chkrootkit -y
+    echo "The following binaries may be malicious:"
+    sudo debsums -ac 2>&1 | grep -v missing
 
 elif [ "$operatingSystem" = "centos" ]; then
     echo "CentOS detected, using yum"
-    sudo yum install epel-release -y #All files required for installation of RKHunter are contained in the EPEL repository.
-    sudo yum install rkhunter -y
+
+elif [ "$operatingSystem" = "fedora" ]; then
+    echo "CentOS detected, using "
+
+elif [ "$operatingSystem" = "openbsd" ]; then
+    echo "OpenBSD detected, using "
+
 fi
-
-#rkhunter
-# You have to config rkhunter.conf in order to use 
-sudo sed -i 's/^MIRRORS_MODE=1/MIRRORS_MODE=0/' /etc/rkhunter.conf
-sudo sed -i 's/^UPDATE_MIRRORS=0/UPDATE_MIRRORS=1/' /etc/rkhunter.conf
-sudo sed -i 's|^WEB_CMD="/bin/false"|WEB_CMD=""|' /etc/rkhunter.conf
-
-# Update local rkhunter database
-rkhunter --update
-
-# Run rkhunter rootkit checks
-rkhunter --check --skip-keypress | grep 'Warning'
-
-# It is recommended to also include a chkrootkit check after rkhunter as the latter is unable to identify everything
-# chkrootkit
-
-
-#debsums
-
-
-#clamav
