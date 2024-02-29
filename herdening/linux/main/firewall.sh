612753ef6b946bbece12c07d06f4ca735a737bf8
johntrigg <60859958+johntrigg@users.noreply.github.com>
2024-02-29 17:55:09 -0500

    Added udp DNS exception to fw

--- herdening/linux/main/firewall.sh
+++ herdening/linux/main/firewall.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
 sudo ufw disable
-# Define the array of ports to open
-open_ports=(80 443 22 3306 21 20) # Add your desired ports here
+
+# Define arrays of TCP and UDP ports to open
+tcp_ports=(80 443 22 3306 21 20) # Add your desired TCP ports here
+udp_ports=(53) # Add your desired UDP ports here
 
 # Set default policies
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 
-# Loop through the open_ports array and allow each port
-for port in "${open_ports[@]}"; do
-    sudo ufw allow $port
+# Loop through the tcp_ports array and allow each TCP port
+for port in "${tcp_ports[@]}"; do
+    sudo ufw allow $port/tcp
+done
+
+# Loop through the udp_ports array and allow each UDP port
+for port in "${udp_ports[@]}"; do
+    sudo ufw allow $port/udp
 done
 
 # Enable UFW
 sudo ufw enable
 
+# Restart UFW to apply changes
 sudo ufw restart
 
 # Check the status of UFW
