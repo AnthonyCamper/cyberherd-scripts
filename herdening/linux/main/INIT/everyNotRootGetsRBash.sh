f6fce14b418705511dec4e1b23889d05add7a285
gerbsec <gerbsec@proton.me>
2024-03-03 16:03:52 -0500

    nevin

--- herdening/linux/main/INIT/everyNotRootGetsRBash.sh
+++ herdening/linux/main/INIT/everyNotRootGetsRBash.sh
@@ -29,33 +29,33 @@ while IFS=: read -r username _ _ _ _ home shell; do
     echo 'PATH=/usr/local/rbin' >> "$home"/.*shrc # Change to a safe, restricted bin directory
     echo 'export PATH' >> "$home"/.*shrc
     
-    # Create a restricted bin directory if it doesn't exist and populate it with allowed commands
-    mkdir -p /usr/local/rbin
-    chown root:root /usr/local/rbin
-    chmod 755 /usr/local/rbin
-    # Link necessary commands here, e.g., ln -s /bin/ls /usr/local/rbin/ls
-    ln -s /usr/bin/whoami /usr/local/rbin/whoami
-    ln -s /usr/bin/id /usr/local/rbin/id
-
-    # Ensure the symbolic links are owned by root and not writable by others
-    chown root:root /usr/local/rbin/whoami
-    chown root:root /usr/local/rbin/id
-    chmod 755 /usr/local/rbin/whoami
-    chmod 755 /usr/local/rbin/id
     
     if command -v apk >/dev/null; then
         echo 'export PATH=/usr/local/rbin' >> "$home"/.profile # Ensure PATH is restricted even here
     fi
-    
+
+    # Ensure users cannot execute files from their home directory
+    chmod -R go-w "$home"
+    find "$home" -type d -exec chmod go+x {} +  # Allow directory traversal but not execution    
     # Make .shrc files immutable to prevent changes
     sudo chattr +i "$home"/.*shrc 2>/dev/null
     
-    # Ensure users cannot execute files from their home directory
-    chmod -R go-w "$home"
-    find "$home" -type d -exec chmod go+x {} +  # Allow directory traversal but not execution
   fi
 done < /etc/passwd
 
+# Create a restricted bin directory if it doesn't exist and populate it with allowed commands
+mkdir -p /usr/local/rbin
+chown root:root /usr/local/rbin
+chmod 755 /usr/local/rbin
+# Link necessary commands here, e.g., ln -s /bin/ls /usr/local/rbin/ls
+ln -s /usr/bin/whoami /usr/local/rbin/whoami
+ln -s /usr/bin/id /usr/local/rbin/id
+
+# Ensure the symbolic links are owned by root and not writable by others
+chown root:root /usr/local/rbin/whoami
+chown root:root /usr/local/rbin/id
+chmod 755 /usr/local/rbin/whoami
+chmod 755 /usr/local/rbin/id
 
 
 echo "Shell change process completed."
