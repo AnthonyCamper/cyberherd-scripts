72196d5cc29a87da472e1862c70e264bd3ab9194
Cameron <93406060+YameronB@users.noreply.github.com>
2024-03-07 19:14:01 -0500

    这是一个混合的字符串包含中文和Кириллица字符

--- herdening/windows/All/DnsHarden.ps1
+++ herdening/windows/All/DnsHarden.ps1
@@ -6,17 +6,15 @@ If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 }
 
 # Backup DNS Configuration
-Export-Clixml -Path "C:\Backup\DNSConfigBackup.xml" -InputObject (Get-DnsServerZone)
+Export-Clixml -Path "C:\DNSBackup\DNSConfigBackup.xml" -InputObject (Get-DnsServerZone)
 
 # Restore DNS Configuration
-# Import-Clixml -Path "C:\Backup\DNSConfigBackup.xml" | ForEach-Object { Set-DnsServerZone $_ }
+# Import-Clixml -Path "C:\DNSBackup\DNSConfigBackup.xml" | ForEach-Object { Set-DnsServerZone $_ }
 
 # Mitigate LLMNR Poisoning
 # This setting is not directly available via PowerShell for all Windows versions, often managed via Group Policy:
 # Computer Configuration -> Administrative Templates -> Network -> DNS Client -> Turn off Multicast Name Resolution
 # Set the policy to Enabled to mitigate LLMNR Poisoning
 
-# Disable IPv6
-# This action is impactful and should be carefully considered before implementation
-# Disable IPv6 on all nontunnel interfaces (not recommended without thorough testing)
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" -Name "DisabledComponents" -Value 0xFF
+# Disable IPv6 on all adapters
+Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6
