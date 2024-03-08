21805038f5b862c120eca995346632175a71dce2
YameronB <93406060+YameronB@users.noreply.github.com>
2024-03-08 16:33:11 -0500

    Final changes and consolidations

--- herdening/windows/All/SmbHarden.ps1
+++ herdening/windows/All/SmbHarden.ps1
@@ -1,8 +1,5 @@
 # SMB Hardening
 
-# Update SMB to the latest version (Ensure Windows is up to date)
-Write-Output "Please ensure Windows is up to date to have the latest SMB version."
-
 # Disable SMBv1
 Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
 
@@ -16,10 +13,12 @@ Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Par
 # Enforce NTLMv2
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LmCompatibilityLevel" -Value 5
 
-# Disable execution from shares (This requires setting the appropriate NTFS permissions on shared folders)
-Write-Output "Ensure that NTFS permissions on shared folders are set to prevent execution."
+# Disable NetBIOS over TCP/IP
+$base = "HKLM:SYSTEM\CurrentControlSet\Services\NetBT\Parameters\Interfaces"
 
-# Disable NetBIOS over TCP/IP (This might require a restart and affects network communications)
-Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object {$_.TcpipNetbiosOptions -ne 2} | ForEach-Object {
-    $_.SetTcpipNetbios(2)
+$interfaces = Get-ChildItem $base | Select -ExpandProperty PSChildName
+
+# To revert change, run this and set value to 0
+foreach($interface in $interfaces) {
+    Set-ItemProperty -Path "$base\$interface" -Name "NetbiosOptions" -Value 2
 }
